fe236780f2826eecc62758e9b86c2669305b3836
郑莹莹 <zhengyingying@playcrab.com>
2017-03-28 17:45:34 +0800

    修改当前版本

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ with open('README.rst') as fp:
 
 setup(
     name = 'rake-workflow',
-    version = '1.1.1',
+    version = '1.1.3',
     keywords = ('rake', 'paramiko','workflow','ansible'),
     description = 'devops use workflow to  Batch Management System and Production Application ',
 
